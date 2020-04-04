cd9580baa570a941c8bef749cb45be11186c0056
leijing000 <1084961608@qq.com>
2020-04-04 16:54:37 +0800

    返回值封装

---
+++ dcloud/src/main/java/com/example/dcloud/entity/Result.java
@@ -0,0 +1,31 @@
+package com.example.dcloud.entity;
+
+public class Result<T> {
+    private Integer code;//1成功，0失败
+    private String msg;//返回提示
+    private T data;//返回数据
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
