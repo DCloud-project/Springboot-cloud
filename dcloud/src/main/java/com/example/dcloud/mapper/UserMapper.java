8c1233fd340f54036082214b49a474fe7c21f242
chenwenju <31333379+chenwenju@users.noreply.github.com>
2020-04-24 10:12:23 +0800

    修改用户管理

--- dcloud/src/main/java/com/example/dcloud/mapper/UserMapper.java
+++ dcloud/src/main/java/com/example/dcloud/mapper/UserMapper.java
@@ -27,49 +27,49 @@ import java.util.Map;
 public interface UserMapper extends BaseMapper<User> {
     @Select("SELECT email FROM user WHERE email = #{email}")
     String checkEmail(String account);  //查询账号
-    @Insert("INSERT INTO user (name,nickname,image,sno,sex,telphone,email,password,birth,roleId,exp,state,schoolId," +
-            "powerId,education,isDelete) VALUES('0','0','0','0','0','0',#{email},#{password},'0','0','0','0'," +
+    @Insert("INSERT INTO user (name,nickname,image,sno,sex,telphone,email,password,birth,role_id,exp,state,school_id," +
+            "power_id,education,is_delete) VALUES('0','0','0','0','0','0',#{email},#{password},'0','0','0','0'," +
             "'0','0','0','0')")  //插入新账号
     void addUser(String email,String password);
     @Select("SELECT password FROM user WHERE email = #{email}")
     String checkPassword(String account);  //查询密码
 
-    @Select("SELECT roleId FROM user WHERE email = #{email}")
+    @Select("SELECT role_id FROM user WHERE email = #{email}")
     int selRole(String account);  //查询角色
 
-    @Select("SELECT name,sex,email,roleId,state,schoolId FROM user WHERE isDelete = 0 AND roleId != 2 LIMIT #{offset},10")
+    @Select("SELECT name,sex,email,role_id,state,school_id FROM user WHERE isDelete = 0 AND role_id != 2 LIMIT #{offset},10")
     List<Map>selUser(int offset);  //查询用户列表
 
-    @Insert("INSERT INTO user (name,nickname,image,sno,sex,telphone,email,password,birth,roleId,exp,state,schoolId," +
-            "powerId,education,isDelete) VALUES(#{name},'0','0','0',#{sex},'0',#{email},#{password},'0',#{roleId},'0','0'," +
+    @Insert("INSERT INTO user (name,nickname,image,sno,sex,telphone,email,password,birth,role_id,exp,state,school_id," +
+            "power_id,education,is_delete) VALUES(#{name},'0','0','0',#{sex},'0',#{email},#{password},'0',#{roleId},'0','0'," +
             "'0','0','0','0')")  //管理员新增用户
     void addUserByAdmin(String name,int sex,String email,String password,int roleId);
 
-    @Update("UPDATE user SET name = #{name},sex = #{sex},roleId = #{roleId} WHERE email = #{email}")
+    @Update("UPDATE user SET name = #{name},sex = #{sex},role_id = #{roleId} WHERE email = #{email}")
     void updUserByAdmin(String name,int sex,int roleId,String email);
 
-    @Update("UPDATE user SET isDelete = 1 WHERE email = #{email}")
+    @Update("UPDATE user SET is_delete = 1 WHERE email = #{email}")
     void delUser(String email);
 
-    @Select("SELECT COUNT(*) FROM user where isDelete = 0")
+    @Select("SELECT COUNT(*) FROM user where is_delete = 0")
     int selUserNum();  //查询用户数
 
-    @Select("SELECT name,sex,email,roleId,state,schoolId FROM user WHERE isDelete = 0 AND roleId != 2 AND state = #{state} LIMIT #{offset},10")
+    @Select("SELECT name,sex,email,role_id,state,school_id FROM user WHERE is_delete = 0 AND role_id != 2 AND state = #{state} LIMIT #{offset},10")
     List<Map>selUserByState(int state,int offset);  //根据状态查询用户
 
-    @Select("SELECT name,sex,email,roleId,state,schoolId FROM user WHERE isDelete = 0 AND roleId != 2 AND name LIKE #{text} LIMIT #{offset},10")
+    @Select("SELECT name,sex,email,role_id,state,school_id FROM user WHERE is_delete = 0 AND role_id != 2 AND name LIKE #{text} LIMIT #{offset},10")
     List<Map>selUserByName(@Param("text")String text,int offset);  //根据用户名查询用户
 
-    @Select("SELECT name,sex,email,roleId,state,schoolId FROM user WHERE isDelete = 0 AND roleId != 2 AND name LIKE #{text} AND state = #{state} LIMIT #{offset},10")
+    @Select("SELECT name,sex,email,role_id,state,school_id FROM user WHERE is_delete = 0 AND role_id != 2 AND name LIKE #{text} AND state = #{state} LIMIT #{offset},10")
     List<Map>selUserByNameAndState(@Param("text")String text,int state,int offset);  //根据用户名查询用户
 
-    @Select("SELECT COUNT(*) FROM user WHERE isDelete = 0 AND roleId != 2 AND state = #{state}")
+    @Select("SELECT COUNT(*) FROM user WHERE is_delete = 0 AND role_id != 2 AND state = #{state}")
     int selUserByStateNum(int state,int offset);  //根据状态查询用户
 
-    @Select("SELECT COUNT(*) FROM user WHERE isDelete = 0 AND roleId != 2 AND name LIKE #{text}")
+    @Select("SELECT COUNT(*) FROM user WHERE is_delete = 0 AND role_id != 2 AND name LIKE #{text}")
     int selUserByNameNum(@Param("text")String text,int offset);  //根据用户名查询用户
 
-    @Select("SELECT COUNT(*) FROM user WHERE isDelete = 0 AND roleId != 2 AND name LIKE #{text} AND state = #{state}")
+    @Select("SELECT COUNT(*) FROM user WHERE is_delete = 0 AND role_id != 2 AND name LIKE #{text} AND state = #{state}")
     int selUserByNameAndStateNum(@Param("text")String text,int state,int offset);  //根据用户名查询用户
 
     @Update("UPDATE user SET state = #{state} WHERE email = #{email}")
