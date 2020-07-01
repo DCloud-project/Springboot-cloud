6300101b15e6df647d84239cf4cc53730cdcffbd
chenwenju <31333379+chenwenju@users.noreply.github.com>
2020-07-01 21:14:22 +0800

    更新注册

--- dcloud/src/main/java/com/example/dcloud/controller/RegisterController.java
+++ dcloud/src/main/java/com/example/dcloud/controller/RegisterController.java
@@ -2,8 +2,11 @@ package com.example.dcloud.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.dcloud.annotation.NoToken;
+import com.example.dcloud.entity.User;
 import com.example.dcloud.service.UserService;
+import com.example.dcloud.util.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -27,14 +30,36 @@ public class RegisterController {
         Map map = JSON.toJavaObject(jsonObject,Map.class);
         String email = (String) map.get("email");
         String password = (String) map.get("password");
-        JSONObject jsonObject1 = new JSONObject();
-        if(userService.registerService(email,password)==1){
-            jsonObject1.put("respCode","1");
-            return jsonObject1.toString();
+        User user = new User();
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        user.setEmail(email);
+        int count = userService.count(queryWrapper);
+        if(count==1){
+            return ResultUtil.error("该邮箱已经注册过了！");
         }
         else{
-            jsonObject1.put("respCode","账号已存在");
-            return jsonObject1.toString();
+            User user1 = new User();
+            user1.setEmail(email);
+            user1.setExp(0);
+            user1.setIsDelete(0);
+            user1.setNickname("0");
+            user1.setSno("0");
+            user1.setName("0");
+            user1.setImage("0");
+            user1.setBirth("0");
+            user1.setTelphone("0");
+            user1.setSex(0);
+            user1.setEducation(0);
+            user1.setPassword(password);
+            user1.setPowerId("0");
+            user1.setSchoolCode("0");
+            user1.setState(0);
+            if(map.get("role_id")==null){
+                user1.setRoleId(0);
+            }else{
+                user1.setRoleId(3);
+            }
+            return ResultUtil.success();
         }
     }
 }
